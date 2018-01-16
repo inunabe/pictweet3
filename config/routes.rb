d3575ffe4866ae4d91427a67530a0fc4273d9bcd
福栄肥料株式会社 <fukuei@fukuehiryoukabushikikaishanoMacBook-Air.local>
2018-01-16 13:06:40 +0900

    新規投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :tweets
+  resources :tweets #コントローラーごとに設定
   #     get 'tweets' => 'tweets#index'
   #     get 'tweets' => 'tweets#new'
+  #     post 'tweets' => 'tweets#create'
 end
