d805f26d0b9aea593f39b4252cabb20cd39ade64
福栄肥料株式会社 <fukuei@fukuehiryoukabushikikaishanoMacBook-Air.local>
2018-01-16 16:15:48 +0900

    ログイン機能を実装した

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,10 @@
 class TweetsController < ApplicationController
+
+  before_action :move_to_index, except: :index
+  # except(以外)index以外の全てのアクションより早くmove_to_indexを実行する
+  # exceptがないとmove_to_indexを実行した後、indexに行かず、move_to_indexを永遠ループする
+
+
   def index
       @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -13,5 +19,11 @@ class TweetsController < ApplicationController
   private
   def tweet_params
     params.permit(:name,:image,:text)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
