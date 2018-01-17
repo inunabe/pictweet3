392e4f233fec5f37835eeb66784d1e3edf81cb77
福栄肥料株式会社 <fukuei@fukuehiryoukabushikikaishanoMacBook-Air.local>
2018-01-17 17:03:33 +0900

    削除機能を実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,6 +16,13 @@ class TweetsController < ApplicationController
     Tweet.create(image:tweet_params[:image],text:tweet_params[:text],user_id:current_user.id)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.destroy
+    end
+  end
+
   private
   def tweet_params
     params.permit(:image,:text)
@@ -23,6 +30,7 @@ class TweetsController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
+      # unless文  返り値がfalseのときに処理を行う
       redirect_to action: :index
     end
   end
