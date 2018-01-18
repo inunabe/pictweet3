4cdbc686c60e79a87fe678602f637f36932a31b5
福栄肥料株式会社 <fukuei@fukuehiryoukabushikikaishanoMacBook-Air.local>
2018-01-18 12:00:13 +0900

    コメント機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -34,6 +34,7 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
+    @comment = @tweet.comments.includes(:user)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets #コントローラーごとに設定
+  resources :tweets do   #コントローラーごとに設定
+    resources :comments,only: [:create]
+    #  post 'tweets/:tweet_id/comment' => 'comments#create'
+  end
+  resources :users,only: [:show]
   #     get 'tweets' => 'tweets#index'
   #     get 'tweets/new' => 'tweets#new'
   #     post 'tweets' => 'tweets#create'
-  get 'users/:id' => 'users#show'
+  # get 'users/:id' => 'users#show'
+
   # delete 'tweets/:id' => 'tweets#destroy'
   # get 'tweets/:id/edit' => 'tweets#edit'
   # get 'tweets/:id' => 'tweets#update'
